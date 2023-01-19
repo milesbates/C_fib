f9ccf824747df838dcc6217a0a57bf9e04b72641
batesm <103465245+milesbates@users.noreply.github.com>
2023-01-19 16:11:43 +0000

    Initial commit.

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
-int main(void){
-    printf(fib(10));
-}
+
 
 int fib(int inp){
-    int num=0;
-    int num2=1;
-    int sub=0;
-    int rv=0;
+    int num;
+    int num2;
+    int sub;
+    int rv;
+    num=0;  
+    num2=0;
+    sub=0; 
+    rv=0;
     for(int i=0; i<inp; i++){
         sub = num;
         num = num+num2;
@@ -14,4 +16,9 @@ int fib(int inp){
         rv = num+rv;
     }
     printf("%d",rv);
+  return 0;
+}
+
+int main(void){
+    fib(10);
 }
